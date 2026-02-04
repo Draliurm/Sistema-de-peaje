e78dff29c5e54e78ae5c086f04ca9c1da8abccb4
Draliurm <Draliurm@gmail.com>
2026-02-04 10:10:04 -0600

    Feat: Time of day

--- main.py
+++ main.py
@@ -8,6 +8,12 @@ match veiculos:
         print("puede pasar")
     case _:
         print("Acceso denegado")
-# hora= float(input("Ingrese le hora: "))
+hora= float(input("Ingrese le hora: "))
+if hora>6 and hora<9 or hora>17 and hora<20:
+    print("Se le aplicara un cargo extra")
+else:
+    print("No se le aplicara ningun cargo extra")
+
 # telepas= input("Posee telepass? ")
+
 
