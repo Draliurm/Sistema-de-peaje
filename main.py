87d5b69aaeb3a0c7d32da6d56f37bfc8a31e1851
Draliurm <Draliurm@gmail.com>
2026-02-04 11:10:21 -0600

    feat: Motorcycle validation

--- main.py
+++ main.py
@@ -1,20 +1,27 @@
 veiculos= input("Ingrese el veiculo: ")
+hora= float(input("Ingrese le hora: "))
+telepass= input("Posee telepass? ")
+moto=5
+auto=10
+camion=20
 match veiculos:
     case "moto":
-        print("puede pasar")
+        if hora>6 and hora<9 or hora>17 and hora<20:
+            moto+=20
+        if telepass == "si":
+            moto-=2
+        print(f"Se autoriza el paso con un valor de: {moto}")    
     case "auto":
-        print("puede pasar")
+        if hora>6 and hora<9 or hora>17 and hora<20:
+            print("Se le aplicara un cargo extra")
+        else:
+            print("No se le aplicara ningun cargo extra")
+            print("puede pasar")
     case "camion":
-        print("puede pasar")
+        if hora>6 and hora<9 or hora>17 and hora<20:
+            print("Se le aplicara un cargo extra")
+        else:
+            print("No se le aplicara ningun cargo extra")
+            print("puede pasar")
     case _:
         print("Acceso denegado")
-hora= float(input("Ingrese le hora: "))
-if hora>6 and hora<9 or hora>17 and hora<20:
-    print("Se le aplicara un cargo extra")
-else:
-    print("No se le aplicara ningun cargo extra")
-telepass= input("Posee telepass? ")
-if telepass == si or no:
-    print("Se le aplicara un decuento de 2$")
-else:
-    print("No se le aplicara ningun descuento")
